acbff7ea6fb62aa623b881fe3c644472c107d99b
Sathish Srinivasalu <7663.Srinivasalu@opt.in>
2024-09-16 21:14:53 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,87 @@
+// let Name = "Sathish";
+let Name = "sathish jana";
+// for (let i = Name.length; i >= 0 ; i--) {
+//    result += Name.charAt(i)
+    
+// }
+// console.log(result)
+
+// for (let i = Name.length; i >= 0 ; i--) {
+//     result += Name.charAt(i)
+     
+//  }
+//  console.log(result == Name)
+// console.log(Name.split('').reverse().join(''));
+
+// console.log(Name == Name.split('').reverse().join(''));
+
+// vowels in string 
+// let count = 0;
+// let vowels = (str)=>{
+//     if(str == 'a' ||str == 'e'||str == 'i'||str == 'o'|| str == 'u'){
+//         return true;
+//     }
+
+// }
+// for (let i = 0; i < Name.length; i++) {
+//     if(vowels(Name.charAt(i))){
+//         count++;
+//     }else{
+//         continue;
+//     }
+// }
+// console.log(count)
+
+// first letter capital 
+
+let first = 'hello world';
+
+let values = first.split(' ')
+// console.log(values)
+// for (let i = 0; i < values.length; i++) {
+//     values[i] = values[i].charAt(0).toUpperCase() + values[i].slice(1)
+//     console.log(values[i])
+// }
+// console.log(values.join(' '))
+
+// longest word
+
+// first = ''
+// for (let i = 0; i < values.length; i++) {
+//    if(first.length < values[i].length){
+//     first = values[i];
+//    }
+    
+// }
+// console.log(first)
+
+//remove duplicates
+// let result = '';
+// for (let i = 0; i < Name.length; i++) {
+//     if(result.indexOf(Name.charAt(i)) == -1){
+//         result += Name.charAt(i);
+//     }  
+// }
+// console.log(result)
+
+// count of word
+
+// console.log(Name.split(' ').length)
+
+// _ adding
+
+// let ana = 'sathish jana'
+
+// console.log(ana.split(' ').join('_'));
+
+// anagram
+
+let ana = 'sathish';
+
+console.log(ana.split('').sort())
+ana.includes()
+
+
+
+
+
